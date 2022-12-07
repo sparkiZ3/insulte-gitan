93de3d4eacc7b32a5867cea1262ec9206c54670b
sparkiZ3 <77058193+sparkiZ3@users.noreply.github.com>
2022-12-07 16:59:42 +0100

    Update script.py
    optimize program

--- script.py
+++ script.py
@@ -1,9 +1,7 @@
 import random
 fichier = open("data.txt", "r") #ouverture du fichier de donnée
-masculin_recup=fichier.readline() #on recupere la premiere ligne qui correspond aux mots masculins
-feminin_recup=fichier.readline() #on recupere la seconde ligne qui correspond aux mots féminin
-masculin=masculin_recup.split() #on met les mots dans une liste avec la fonction split()
-feminin=feminin_recup.split()
+masculin=fichier.readline().split() #on met les mots dans une liste avec la fonction split()
+feminin=fichier.readline().split()
 if random.randint(1, 2)==1: #choix aléatoire féminin ou masculin
     print("le "+random.choice(masculin)+" de tes morts") #on affiche la phrase au masculin
 else:
